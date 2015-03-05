3b9eeacacef254003c3019d0dc14de7115273bed
ervios <evilirek@gmail.com>
2015-03-05 20:54:26 +0100

    ASdf

--- bartender/bartender.js
+++ bartender/bartender.js
@@ -4,15 +4,8 @@ function newUserButton() {
 }
 
 
-function addNewUser() {
-
-	var fname 	= document.getElementById("firstname").value;
-	var lname 	= document.getElementById("lastname").value;
-	var uname 	= document.getElementById("username").value;
-	var pass 	= document.getElementById("password").value;
-	var email 	= document.getElementById("email").value;
-	var phone 	= document.getElementById("phone").value;
- 	
+function addNewUser(fname, lname, uname, pass, email, phone) {
+	 	
  	var db_link = getDBData('user_edit');
  	
  	db_link += "&new_username="
@@ -35,29 +28,60 @@ function addNewUser() {
     	dataType: 'json',
     	success: function(data) {
     		console.log(data);
-    		$("ul").append("<li>ADDED USER!<br> " 
-   							+ uname
-   							+ "</li><li>First Name: "
-   							+ fname
-   							+ "</li><li>Last Name: "
-   							+ lname
-   							+ "</li><li>User Name: "
-   							+ pass
-   							+ "<br>-------"
-   							);
+    		$("ul").append(
+    			"<li>ADDED USER!<br> " 
+   				+ uname
+   				+ "</li><li>First Name: "
+   				+ fname
+   				+ "</li><li>Last Name: "
+   				+ lname
+   				+ "</li><li>Password: "
+   				+ pass
+   				+ "<br>-------"
+   			);
  		}
  	});
  	
 }
 
-function validateNewUserInfo(tmpValue) {
+function validateInput() {
 
-	if(tmpValue != "" && tmpValue!= null) {
-		return true;
-	} else {
-		return false;
+	var check = 0;
+
+	var fname 	= document.getElementById("firstname").value;
+	var lname 	= document.getElementById("lastname").value;
+	var uname 	= document.getElementById("username").value;
+	var pass 	= document.getElementById("password").value;
+	var email 	= document.getElementById("email").value;
+	var phone 	= document.getElementById("phone").value;
+
+	if(fname !== "" && fname!== 0) {
+		check += 1;
+	}
+	if(lname !== "" && lname!== 0) {
+		check += 1;
+	}
+	if(uname !== "" && uname!== 0) {
+		check += 1;
+	}
+	if(pass !== "" && pass!== 0) {
+		check += 1;
+	}
+	if(email !== "" && email!== 0) {
+		check += 1;
+	}
+	if(phone !== "" && phone!== 0) {
+		check += 1;
+	}
+	else {
+		alert("smthing empty");
 	}
 
+	if(check == 6) {
+		alert(check);
+		addNewUser(fname, lname, uname, pass, email, phone);
+	}
+	
 }
 
 function updateNewUser() {
